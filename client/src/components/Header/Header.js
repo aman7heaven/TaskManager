261649b5c775faa28102b0ec4239143e10529a55
Aman <amansaxena.4654@gmail.com>
2023-10-03 13:01:28 +0530

    made changes in navbar

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -9,47 +9,41 @@ import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../../actions/userActions';
 
-
 const Header = () => {
-
   const navigate = useNavigate();
-
   const dispatch = useDispatch();
-
-  const userLogin = useSelector((state)=>state.userLogin);
-  const {userinfo} = userLogin;
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userinfo } = userLogin;
 
   const logoutHandler = () => {
     dispatch(logout());
-    navigate("/");
-  }
+    navigate('/');
+  };
 
   return (
     <Navbar expand="lg" bg="primary" data-bs-theme="dark">
-    <Container>
-      <Navbar.Brand>
-        <Link to='/'>Task Manager</Link>
+      <Container>
+        <Navbar.Brand>
+          <Link to="/">Task Manager</Link>
         </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link>
-          <Link to="/mytasks">My Tasks</Link>
-          </Nav.Link>
-          <NavDropdown title="Aman Saxena" id="basic-nav-dropdown" bg="light">
-          {/* <NavDropdown.Item href="#action/3.4" backgroundColour="light">
-              My Profile
-            </NavDropdown.Item> */}
-            {/* <NavDropdown.Divider /> */}
-            <NavDropdown.Item onClick={logoutHandler}>
-              Logout
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-  )
-}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            {userinfo ? (
+              <>
+                <Nav.Link>
+                  <Link to="/mytasks">My Tasks</Link>
+                </Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown" bg="light">
+                  <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                </NavDropdown>
+              </>
+            ) : null}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default Header
+export default Header;
